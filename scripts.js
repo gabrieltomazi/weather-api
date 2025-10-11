f0a91dad2d8b26be0c855c840cd3d7af8d6eba2e
gabrieltomazi <tomazigabriel3@gmail.com>
2025-10-11 16:41:56 -0300

    algumas mudancas

--- scripts.js
+++ scripts.js
@@ -1,27 +1,27 @@
 
 let chave = "8eca2540b9c448a8a2d184036251110"
 
-function colocaNaTela(dados) {
+function showOnScreen(dados) {
     console.log(dados)
     document.querySelector(".cidade").innerHTML = `Tempo em ${dados.location.name}`
     document.querySelector(".temp").innerHTML = `${dados.current.temp_c}°C`
     document.querySelector(".description").innerHTML = `${dados.current.condition.text}` 
     document.querySelector(".umidade").innerHTML = `Umidade: ${dados.current.humidity}%`
     document.querySelector(".icon").src = `${dados.current.condition.icon}`
-    // 
+
 }
 
 
-async function buscarCidade(cidade) {
-    let dados = await fetch(`https://api.weatherapi.com/v1/current.json?key=${chave}&q=${cidade}&aqi=yes&lang=pt&units=metrics`).then(resposta => resposta.json())
-    colocaNaTela(dados)
+async function findCity(city) {
+    let dados = await fetch(`https://api.weatherapi.com/v1/current.json?key=${chave}&q=${city}&aqi=yes&lang=pt&units=metrics`).then(resposta => resposta.json())
+    showOnScreen(dados)
 }
 
 
-function cliqueiNoBotao() {
-    let cidade = document.querySelector("#input-cidade").value
-    buscarCidade(cidade)
+function getCity() {
+    let city = document.querySelector("#input-cidade").value
+    findCity(city)
 }
 
 
-buscarCidade('Criciúma')
+findCity('Criciúma')
